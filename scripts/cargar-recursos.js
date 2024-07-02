aa862871968aab0acc016cfaa17cc8ba5a500ed4
Rocio Fernandez <97984182+ro-fer@users.noreply.github.com>
2024-07-01 21:04:31 -0300

    Actualizacion

--- scripts/cargar-recursos.js
+++ scripts/cargar-recursos.js
@@ -1,59 +1,19 @@
-<<<<<<< Updated upstream
-const resources = await response.json();
-    
-const filteredResources = resources.filter(resource => resource.categorias.includes(categoria));
-console.log('Recursos filtrados:', filteredResources);
-
-filteredResources.forEach(resource => {
-  insertCard(resource);
-});
-} catch (error) {
-console.error('Error:', error);
-}
-}
-function createCard(resource) {
-const divCard = document.createElement('div');
-divCard.classList.add('recurso');
-
-const { nombre, link, descripcion, categorias } = resource;
-divCard.innerHTML = `
-<h3 class="recurso-title">${nombre}</h3>
-<div class="recurso-descripcion">
-  ${descripcion}
-  <div class="recurso-boton">
-    <a href="${link}" target="_blank">Sitio Recurso</a>
-  </div>
-  <div class="categorias">
-    ${categorias.map(categoria => `
-      <div class="categoria">
-        <a href="#" target="_blank">${categoria}</a>
-      </div>`).join('')}
-  </div>
-</div>
-`;
-
-return divCard;
-}
-
-function insertCard(resource) {
-const container = document.getElementById('recursos-container');
-const card = createCard(resource);
-container.appendChild(card);
-=======
 async function cargarRecursos(categoria) {
   try {
     console.log('Categoría:', categoria);
 
     // Ejemplo de fetch y procesamiento de recursos
-    const response = await fetch("../scripts/recursos.json");
+    const response = await fetch("../scripts/recursos.json"); // Ruta relativa desde la raíz del servidor
     if (!response.ok) {
       throw new Error('Failed to fetch recursos.json');
     }
     const resources = await response.json();
-    
+
+    // Filtrar recursos por categoría
     const filteredResources = resources.filter(resource => resource.categorias.includes(categoria));
     console.log('Recursos filtrados:', filteredResources);
 
+    // Insertar las tarjetas de recursos filtrados en el contenedor
     filteredResources.forEach(resource => {
       insertCard(resource);
     });
@@ -61,6 +21,7 @@ async function cargarRecursos(categoria) {
     console.error('Error:', error);
   }
 }
+
 function createCard(resource) {
   const divCard = document.createElement('div');
   divCard.classList.add('recurso');
@@ -89,5 +50,4 @@ function insertCard(resource) {
   const container = document.getElementById('recursos-container');
   const card = createCard(resource);
   container.appendChild(card);
->>>>>>> Stashed changes
 }
