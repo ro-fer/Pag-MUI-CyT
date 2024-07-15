8407d4eb6ad75bff23dc09a7becc46982fd2d6b0
Rocio Fernandez <97984182+ro-fer@users.noreply.github.com>
2024-07-15 17:57:32 -0300

    Update cargar-pags.js

--- scripts/cargar-pags.js
+++ scripts/cargar-pags.js
@@ -1,44 +1,47 @@
-async function cargarPags() {
-    try {
-      // Fetch y procesamiento de recursos
-      const response = await fetch("../scripts/paginas.json"); // Ruta relativa desde la raíz del servidor
-      if (!response.ok) {
-        throw new Error('Failed to fetch paginas.json');
-      }
-      const resources = await response.json();
-  
-      // Insertar cada recurso en el contenedor
-      resources.forEach(resource => {
-        insertCard(resource);
-      });
-    } catch (error) {
-      console.error('Error:', error);
+async function cargarRecursos() {
+  try {
+    const response = await fetch("scripts/paginas.json"); // Ruta relativa o absoluta según tu configuración
+    if (!response.ok) {
+      throw new Error('Failed to fetch paginas.json');
     }
+    const resources = await response.json();
+
+    // Insertar cada recurso en el contenedor
+    resources.forEach(resource => {
+      insertCard(resource);
+    });
+  } catch (error) {
+    console.error('Error:', error);
   }
-  
-  function createCard(resource) {
-    const divCard = document.createElement('div');
-    divCard.classList.add('container');
-  
-    const { nombre, link, descripcion, img } = resource;
-    divCard.innerHTML = `
-      <div class="img">
-        <img src="${img}" alt="${nombre}" class="img-pag">
+}
+
+function createCard(resource) {
+  const divCard = document.createElement('div');
+  divCard.classList.add('container');
+
+  const { nombre, link, descripcion, img } = resource;
+  divCard.innerHTML = `
+    <h4 class="pag-title">${nombre}</h4>
+    <div class="img">
+      <img src="${img}" alt="${nombre}" class="img-pag">
+    </div>
+    <div class="titulo">
+          <h4><a href="${link}" target="_blank" class="link-pag">${nombre}</a></h4>
       </div>
-      <div class="titulo">
-            <h4><a href="${link}" target="_blank" class="link-pag">${nombre}</a></h4>
-        </div>
-        <div class="descripcion">
-            <p>${descripcion}</p>
-        </div>
-    `;
-  
-    return divCard;
-  }
-  
-  function insertCard(resource) {
-    const container = document.getElementById('container-pag');
-    const card = createCard(resource);
-    container.appendChild(card);
-  }
+      <div class="descripcion">
+          <p>${descripcion}</p>
+      </div>
+  `;
+
+  return divCard;
+}
+
+function insertCard(resource) {
+  const container = document.getElementById('container-pag');
+  const card = createCard(resource);
+  container.appendChild(card);
+}
+
+// Llamar a cargarRecursos cuando la página se haya cargado
+document.addEventListener('DOMContentLoaded', cargarRecursos);
 
